096d96461d85e82fa2107cfb7a29c25c180126b6
hina <hina@hinaloe.net>
2016-02-28 08:29:59 +0900

    0.1-ready

--- elementlist.js
+++ elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Push7_Subscribe_Button"],["c","Push7_Subscribe_Button_Widget"],["c","Push7SB_Jetpack"],["c","Push7SubscribeButtoon\\Admin\\Admin"],["c","Push7SubscribeButtoon\\SocialBuzz\\Base"],["c","Push7SubscribeButtoon\\SocialBuzz\\SocialSimple"],["c","Push7SubscribeButtoon\\SocialBuzz\\SocialWithThumb"],["c","Share_Push7"]];
+ApiGen.elements = [["c","Push7_Subscribe_Button"],["c","Push7_Subscribe_Button_Options"],["c","Push7_Subscribe_Button_Widget"],["c","Push7SB_Jetpack"],["c","Push7SubscribeButtoon\\Admin\\Admin"],["c","Push7SubscribeButtoon\\SocialBuzz\\Base"],["c","Push7SubscribeButtoon\\SocialBuzz\\SocialSimple"],["c","Push7SubscribeButtoon\\SocialBuzz\\SocialWithThumb"],["c","Share_Push7"]];
